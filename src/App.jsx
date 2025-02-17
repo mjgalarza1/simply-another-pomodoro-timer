59b5a9e06341e3b69c9b34d981d852ba6da1b444
mjgalarza1 <matias.galarza.unq@gmail.com>
2025-02-17 02:14:54 -0300

    Initial layout and structure setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MainLogo from "./assets/imgs/simply-another-pomodoro-timer-logo.svg";
+import GithubIcon from "./assets/imgs/Octicons-mark-github.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <div id="main-container" className="flex flex-col justify-center items-center gap-1 h-screen w-screen">
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <img id="logo" className="w-lg" src={MainLogo} alt="Simply Another Pomodoro Timer"/>
+
+                <div id="pomodoro-timer-container" className="text-center border-2">
+                    <p>[ WIP: Pomodoro Timer container + bottom buttons ]</p>
+                </div>
+
+                <a
+                    id="github-link"
+                    href="https://github.com/mjgalarza1/simply-another-pomodoro-timer"
+                    target="_blank"
+                    className="fixed p-2 bottom-0 left-0 rounded-tr-xl bg-white flex gap-2">
+                    <img src={GithubIcon} className="w-6"/>
+                    <p>mjgalarza1</p>
+                </a>
+
+            </div>
+
+        </>
+    )
 }
 
 export default App
