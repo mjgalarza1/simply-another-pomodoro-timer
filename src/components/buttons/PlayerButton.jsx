905fea743fd05e851893b2c709ebe2b02df23017
mjgalarza1 <matias.galarza.unq@gmail.com>
2025-02-19 11:56:56 -0300

    Fix PlayerButton click area

--- src/components/buttons/PlayerButton.jsx
+++ src/components/buttons/PlayerButton.jsx
@@ -1,14 +1,16 @@
 const PlayerButton = ({icon,alt,onClick}) => {
     return (
-        <button onClick={onClick} className="
-            w-[48px] max-w-[8vw]
-            transition duration-100 ease-in-out
-            hover:scale-120
-            active:scale-80
-            hover:cursor-pointer
-        ">
-            <img src={icon} alt={alt}/>
-        </button>
+        <div onClick={onClick}>
+            <button className="
+                w-[48px] max-w-[8vw]
+                transition duration-100 ease-in-out
+                hover:scale-120
+                active:scale-80
+                hover:cursor-pointer
+            ">
+                <img src={icon} alt={alt}/>
+            </button>
+        </div>
     )
 }
 
