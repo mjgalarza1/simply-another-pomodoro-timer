792f65f4c61f7c52c98e92e8081a192142ebf577
mjgalarza1 <matias.galarza.unq@gmail.com>
2025-02-19 12:20:38 -0300

    Add logo hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,14 @@ function App() {
     return (
         <>
             <div id="wrapper" className="flex justify-center items-center h-screen min-h-[270px]">
-                <div id="main-container" className="flex flex-col justify-center items-center gap-3 w-[616px] ">
+                <div id="main-container" className="flex flex-col justify-center items-center gap-3 w-[616px]">
 
-                    <img id="logo" className="w-[500px] px-4 [@media(max-height:750px)]:w-[66.5vh] [@media(max-height:350px)]:hidden" src={MainLogo} alt="Simply Another Pomodoro Timer"/>
+                    <div className="overflow-clip">
+                        <img id="logo" className="
+                        w-[500px] px-4 [@media(max-height:750px)]:w-[66.5vh] [@media(max-height:350px)]:hidden
+                        transition duration-300 ease-in-out hover:scale-105 overflow-clip"
+                            src={MainLogo} alt="Simply Another Pomodoro Timer"/>
+                    </div>
 
                     <div id="pomodoro-timer-container">
                         <PomodoroTimer/>
