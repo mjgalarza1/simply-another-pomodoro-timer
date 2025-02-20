b040ec5176b2b6afd1caa0d8b138a2ac42719395
mjgalarza1 <matias.galarza.unq@gmail.com>
2025-02-20 20:51:04 -0300

    Add fade-in animation for PomodoroTimer
    +Refactor: improve naming for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,27 @@ import PomodoroTimer from "./components/PomodoroTimer.jsx";
 function App() {
     return (
         <>
-            <div id="wrapper" className="flex justify-center items-center h-screen min-h-[270px]">
-                <div id="main-container" className="flex flex-col justify-center items-center gap-3 w-[616px]">
+            <div id="main-container" className="flex justify-center items-center h-screen min-h-[270px]">
 
-                    <div className="overflow-clip">
-                        <img id="logo" className="
+                <div id="logo-and-pomodoro-timer-wrapper" className="flex flex-col justify-center items-center gap-3 w-[616px] animate-fadeAndZoomIn">
+
+                    <div id="logo-wrapper" className="overflow-clip">
+                        <img className="
                         w-[500px] px-4 [@media(max-height:750px)]:w-[66.5vh] [@media(max-height:350px)]:hidden
                         transition duration-300 ease-in-out hover:scale-105 overflow-clip"
                             src={MainLogo} alt="Simply Another Pomodoro Timer"/>
                     </div>
 
-                    <div id="pomodoro-timer-container">
+                    <div id="pomodoro-timer-wrapper">
                         <PomodoroTimer/>
                     </div>
 
-                    <div id="github-link" className="fixed bottom-0 left-0">
-                        <GithubButton/>
-                    </div>
                 </div>
+
+                <div id="github-button-wrapper" className="fixed bottom-0 left-0">
+                    <GithubButton/>
+                </div>
+
             </div>
         </>
     )
