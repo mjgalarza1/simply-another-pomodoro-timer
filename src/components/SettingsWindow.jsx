6f81bbaf66be2659726d17c9cecf57cf05aecec4
mjgalarza1 <matias.galarza.unq@gmail.com>
2025-02-25 01:12:57 -0300

    Redesign Settings layout with new colors

--- src/components/SettingsWindow.jsx
+++ src/components/SettingsWindow.jsx
@@ -18,7 +18,7 @@ function SettingsWindow({ close, pomodoroDuration, shortBreakDuration, longBreak
     // Dynamically changes the volume bar color.
     useEffect(() => {
         const volumeSlider = document.getElementById("volume-slider");
-        volumeSlider.style.background = `linear-gradient(to right, #464646 ${alarmVolume * 100}%, #FFFFFF ${alarmVolume * 100}%)`;
+        volumeSlider.style.background = `linear-gradient(to right, #5F6379 ${alarmVolume * 100}%, #FFFFFF ${alarmVolume * 100}%)`;
     }, [alarmVolume]);
 
     // Disables body overflow when SettingsWindow is open
@@ -41,19 +41,19 @@ function SettingsWindow({ close, pomodoroDuration, shortBreakDuration, longBreak
 
     return (
         <div id="modal-container" className={
-            `fixed inset-0 flex justify-center items-center bg-[#4F5B60]/70 z-50 backdrop-blur-[50px]  p-4
+            `fixed inset-0 flex justify-center items-center bg-black/50 backdrop-blur-[35px] z-50 p-4
             ${isOpening ? "animate-settingsFadeIn" : ""}
             ${isClosing ? "animate-settingsFadeOut" : ""}
             `}>
 
             <div id="settings-container" className={
-                `rounded-2xl bg-white shadow-[0_20px_15px_0_rgba(0,0,0,0.4)] my-6 max-[560px]:w-[95vw] max-h-full overflow-y-auto
+                `rounded-2xl bg-[#5F6379] shadow-[0_15px_20px_0_rgba(0,0,0,0.4)] my-6 max-[560px]:w-[95vw] max-h-full overflow-y-auto
                 ${isOpening ? "animate-settingsSlideIn" : ""}
                 ${isClosing ? "animate-settingsSlideOut" : ""}
                 `}>
 
-                <div id="title-wrapper" className="flex flex-row justify-between items-center py-5 ">
-                    <h2 id="title" className="font-fredoka text-4xl tracking-[8px] text-[#464646] pl-8 truncate max-w-full max-[470px]:tracking-[1vw] max-[470px]:text-[18px]">SETTINGS</h2>
+                <div id="title-wrapper" className="flex flex-row justify-between items-center py-5">
+                    <h2 id="title" className="font-fredoka text-4xl tracking-[8px] text-white pl-8 truncate max-w-full max-[470px]:tracking-[1vw] max-[470px]:text-[18px]">SETTINGS</h2>
                     <div id="close-button" onClick={handleClosing} className="w-[26px] h-[26px] min-w-[26px] min-h-[26px] mr-5">
                         <button className="hover:cursor-pointer">
                             <img src={CloseButton} alt="X"/>
@@ -62,7 +62,7 @@ function SettingsWindow({ close, pomodoroDuration, shortBreakDuration, longBreak
                 </div>
 
                 <div id="settings-wrapper"
-                     className="flex flex-col shadow-[inset_0px_4px_4px_0px_rgba(0,0,0,0.15)] px-6 ">
+                     className="flex flex-col shadow-[inset_0px_6px_14px_0px_rgba(95,99,121,0.15)] px-6 bg-white">
                     <div id="timers-wrapper" className="flex flex-row gap-8 py-6 max-[470px]:flex-col">
                         <TimerInput title="Pomodoro" minutes={pomodoroDuration} state={setPomodoroDuration}/>
                         <TimerInput title="Short break" minutes={shortBreakDuration} state={setShortBreakDuration}/>
@@ -70,7 +70,7 @@ function SettingsWindow({ close, pomodoroDuration, shortBreakDuration, longBreak
                     </div>
 
                     <div id="alert-volume-wrapper" className="flex flex-col gap-2 border-t-1 border-[#B1B4C9] py-6 pb-12">
-                        <label htmlFor="volume-slider" className="font-fredoka text-[26px] font-medium text-[#464646] max-[470px]:text-[6.5vw]">
+                        <label htmlFor="volume-slider" className="font-fredoka text-[26px] font-medium text-[#5F6379] max-[470px]:text-[6.5vw]">
                             Alarm volume: {Math.round(alarmVolume * 100)}%
                         </label>
                         <input
@@ -81,7 +81,7 @@ function SettingsWindow({ close, pomodoroDuration, shortBreakDuration, longBreak
                             step="0.01"
                             value={alarmVolume}
                             onChange={handleVolume}
-                            className="w-full h-3 rounded-full border-1 border-[#464646] appearance-none cursor-pointer"
+                            className="w-full h-3 rounded-full border-1 border-[#5F6379] appearance-none cursor-pointer"
                         />
                     </div>
                 </div>
--- src/components/TimerInput.jsx
+++ src/components/TimerInput.jsx
@@ -10,11 +10,11 @@ const TimerInput = ({title,minutes,state}) => {
 
     return (
         <div id="timer-input-container" className="flex flex-col items-center w-[140px] max-[560px]:w-full max-[470px]:items-start">
-            <p className="font-fredoka text-[26px] font-medium text-[#464646] max-[560px]:text-[4.3vw] max-[470px]:text-[16px]">{title}</p>
+            <p className="font-fredoka text-[26px] font-medium text-[#5F6379] max-[560px]:text-[4.3vw] max-[470px]:text-[16px]">{title}</p>
             <input
                 id={title}
                 type="text"
-                className="border p-2 border-[#464646] rounded-[8px] w-full text-center font-fredoka text-[26px] text-[#464646] w-full h-12"
+                className="border p-2 border-[#5F6379] rounded-[8px] w-full text-center font-fredoka text-[26px] text-[#5F6379] w-full h-12"
                 value={minutes}
                 onChange={handleInputChange}
                 inputMode="numeric"
