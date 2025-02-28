5c8d75466b9927eb76144e49d01884368dfe81c1
mjgalarza1 <matias.galarza.unq@gmail.com>
2025-02-28 18:56:18 -0300

    Fix missing name attribute on input

--- src/components/buttons/ToggleButton.jsx
+++ src/components/buttons/ToggleButton.jsx
@@ -3,7 +3,7 @@ const ToggleButton = ({text,onChange,isChecked}) => {
         <div className="flex flex-row gap-2 border-t-1 border-[#B1B4C9] py-6 max-[470px]:flex-col max-[470px]:gap-0">
 
             <label className="inline-flex items-center cursor-pointer">
-                <input type="checkbox" value="" className="sr-only peer" checked={isChecked} onChange={onChange}/>
+                <input type="checkbox" value="" name={text} className="sr-only peer" checked={isChecked} onChange={onChange}/>
                 <div
                     className="relative w-14 h-7 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all peer-checked:bg-[#3688FF]"></div>
             </label>
